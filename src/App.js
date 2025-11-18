41a410df832e372578f7c277f206abd0ab4053d8
johnfinch204 <johnfinch204@outlook.com>
2025-11-18 10:36:19 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to E-ZPassME</h1>
+      <p>Track your toll expenses with ease.</p>
+    </div>
+  );
+}
+
+export default App;
